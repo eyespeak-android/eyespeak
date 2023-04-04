d975758847982efe776b02a4dd6e96ffe4693e99
Jemara Smith <jsmi8824@students.vsu.edu>
2023-04-04 19:18:06 -0400

    updated/edited profile screen and gradle

--- app/src/main/java/com/example/eyespeak/MainActivity.kt
+++ app/src/main/java/com/example/eyespeak/MainActivity.kt
@@ -73,10 +73,10 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EyespeakTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    EyespeakTheme {
+//        Greeting("Android")
+//    }
+//}
